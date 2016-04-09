954eaba03197d76d127574c244a459ba6b43ff94
Swapnil Das <dasswapnil96@gmail.com>
2016-04-09 19:38:36 +0530

    Question from SPOJ based on DFS. Link - http://www.spoj.com/problems/PT07Z/

--- DFS/SP_tree.py
+++ DFS/SP_tree.py
@@ -1,33 +1,57 @@
-graph = {'A': ['B', 'C'],
-             'B': ['C', 'D'],
-             'C': ['D'],
-             'D': ['C'],
-             'E': ['F'],
-             'F': ['C']}
+class tree:
 
-def find_path(graph, start, end, path=[]):
-    
-    path = path + [start]
-    print path
+    def __init__(self, no):
 
-    if start == end:
-        return [path]
-    
-    if not graph.has_key(start):
-        return []
-    
-    paths = []
+        self.no = no
 
-    for node in graph[start]:
+    def LPT(self, s):
+        
+        p1 = 0
+        p2 = 0
+ 
+        for v in self.GRAPH[s]:
             
-        print node
-        if node not in path:
-    
-            newpaths = find_path(graph, node, end, path)
-    
-    	    for newpath in newpaths:
-    	       	paths.append(newpath)
+            if v in self.visited:
+                continue
+ 
+            self.visited.add(v)
+ 
+            value = self.LPT(v) + 1
+ 
+            if value > p1:
+                p1 = value
+                continue
+            
+            elif value == p1 or value > p2:
+                p2 = value
+                continue
+        
+        self.MAX_PATH = max(self.MAX_PATH, p1 + p2)
+       
+        return p1
+ 
+    def make_graph(self):       
+        
+        self.GRAPH = {}
+        self.MAX_PATH = float('-inf')
+        self.visited = set()
+ 
+        if self.no == 1:
+            print 0
+            raise KeyError
+ 
+ 
+        for _ in xrange(1, self.no):
+            
+            u, v = map(int, raw_input().split())
+            self.GRAPH.setdefault(u, []).append(v)
+            self.GRAPH.setdefault(v, []).append(u)
+           
+        start = max(self.GRAPH.keys())
+        self.visited.add(start)
+        self.LPT(start)
+        print self.MAX_PATH
 
-    return paths        
-
-print find_path(graph, 'A', 'D')    
+a = int(raw_input())
+q = tree(a)
+q.make_graph()
